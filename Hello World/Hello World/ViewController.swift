35cc8f4f8ee226de9a8b2a18d6c29f38c9aa2d1c
anshithavelagapudi <anshithavelagapudi@gmail.com>
2020-09-23 19:10:40 -0500

    created Hello World Project

---
+++ Hello World/Hello World/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by student on 9/23/20.
+//  Copyright © 2020 Anshitha Velagapudi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
